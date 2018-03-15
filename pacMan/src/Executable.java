8a839049749fe083975f69a86713ad5c7e94cead
Jack Rosetti <32694046+jackrosetti@users.noreply.github.com>
2018-03-15 14:37:45 -0700

    Update Executable.java

--- pacMan/src/Executable.java
+++ pacMan/src/Executable.java
@@ -1,7 +1,26 @@
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.util.*;
+import java.io.*;
+
 public class Executable {
     //TO DO: Fix kill code, add fruits
     public static void main(String[] args)
     {
         Level1 level1 = new Level1();
+        play();
+    }
+
+    public static void play()
+    {
+        try {
+            File file = new File("sounds//tetris.wav");
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(file));
+            clip.start();
+            Thread.sleep(clip.getMicrosecondLength());
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
